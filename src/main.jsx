dccc031fb5203f92355064cd5ed38b064c043748
Lucas Riera <lucasezequielriera@hotmail.com>
2022-09-12 00:03:30 -0300

    Files saved

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 import { SimpleForm } from './02-useEffect/SimpleForm'
 import { FocusScreen } from './04-useRef/FocusScreen'
-import { MultipleCustomHooks } from './examples/MultipleCustomHooks'
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 import { HooksApp } from './HooksApp'
 import './index.css'
 
